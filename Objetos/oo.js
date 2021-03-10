1c024f972628425fa3a968d9ad7972faa9ad6277
Miguel Costallat <48732887+CostallatP@users.noreply.github.com>
2021-03-10 15:21:26 -0300

    Add files via upload

---
+++ Objetos/oo.js
@@ -0,0 +1,10 @@
+const produto = new Object
+produto.nome='cadeira'
+produto['marca do produto'] = 'generica'
+produto.preco=220
+
+console.log(produto)
+delete produto.preco
+delete produto['marca do produto']
+console.log(produto)
+
